10762d45e43c4b94c1895c20e4488f9c006dab7c
sachin81 <sachin@hubilo.com>
2021-09-11 14:46:50 +0530

    serve file statically

--- app.js
+++ app.js
@@ -20,14 +20,17 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false })); // It will remove the warning but its deprecated
 /* *******REQ BODY PARSING END  *****  */
 
+/* STATIC FILE SERVING ***** */
+app.use(express.static(path.join(__dirname, 'public'))); // Redirect all public file which has to be use in html or in views
+/* *********************** */
+
 // ROUTERS
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 app.use((req, res) => {
   //   res.status(404).send('<h1>Page not found</h1>');
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); // 404 file serve
 });
 
 const server = http.createServer(app);
---
+++ docs/node-basic.md
@@ -0,0 +1,4 @@
+1. Statically Served file : means which can not be handled by express router or other middleware
+                            but instead directly forwarded to the file system
+
+                            
